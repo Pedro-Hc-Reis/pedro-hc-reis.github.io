cffa1076ee0172ffc8dfb025abfd058ce6863546
Pedro-Hc-Reis <pedrohenriquecreis@gmail.com>
2023-09-15 10:24:00 -0300

    v.01

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const menu = document.getElementById('menu');
-  const menuIcon = document.querySelector('.menu-icon');
-  
-  // Adicionar um evento de clique ao ícone do menu
-  menuIcon.addEventListener('click', function () {
-    menu.classList.toggle('open');
+// Verifica se o preloader já foi exibido antes
+const isFirstVisit = sessionStorage.getItem('isFirstVisit') === null;
+
+// Se for a primeira visita, mostra o preloader
+if (isFirstVisit) {
+  const preloader = document.getElementById('preloader');
+  preloader.style.display = 'block';
+
+  // Define um valor no localStorage para indicar que a página foi visitada
+  sessionStorage.setItem('isFirstVisit', 'false');
+
+  // Remove o preloader após o carregamento completo da página com um pequeno atraso
+  window.addEventListener('load', function () {
+    setTimeout(function () {
+      const preloader = document.getElementById('preloader');
+      preloader.style.display = 'none';
+    }, 1000); // Aguarda 1 segundo antes de remover o preloader
   });
-});
+}
+else {
+  const preloader = document.getElementById('preloader');
+  preloader.style.display = 'none';
+}
